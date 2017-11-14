e608a4453970981679ef80010751d51ae9f6677a
bin <bin6160@126.com>
2017-11-14 16:25:38 +0800

    update gradle

---
+++ app/src/main/java/com/dev/bins/note/ui/SigninActivity.kt
@@ -0,0 +1,14 @@
+package com.dev.bins.note.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.dev.bins.note.R
+
+class SignActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign)
+    }
+}
